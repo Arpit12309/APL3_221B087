8b02fcf937cabedf07fe0d2cb43072bfd97fd4ad
221B087 <221B087@juet.co.in>
2025-03-28 09:24:04 +0530

    lab5

---
+++ LE5/Rum.java
@@ -0,0 +1,8 @@
+class Rum extends Beverage{
+    protected void addCondiment(){
+        System.out.println("Add some Ice");
+    }
+    protected void stir(){
+        System.out.println("Stir for 20 sec");
+    }
+}
